1559e41ab8d07cc156e7511e7e2b35c505c6d822
thesmartypants <alex@shmulevich.com>
2023-03-30 18:43:40 -0500

    first version

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ hist = open('history.csv', 'a')
 cur = input('enter crypto currency: ')
 of_cur = input('official currency in your country ex: cad, usd, mxn: ')
 url = f'https://api.coingecko.com/api/v3/simple/price?ids={cur}&vs_currencies={of_cur}'
-while 1:
-  req = urllib.request.urlopen(url).read().decode()
-  d = json.loads(req)
-  last_price = d[cur][of_cur]
-  now = date.today()
-  datetime=now.strftime('%b-%d-%Y-%H-%M-%S')
-  hist.write(datetime+','+last_price+'\n')
-  os.system('clear')
-  print(hist.read())
-  time.sleep(15)
-hist.close()
+
+try:
+  while 1:
+    # get last price
+    req = urllib.request.urlopen(url).read().decode()
+    d = json.loads(req)
+    last_price = d[cur][of_cur]
+
+    #add to history file
+    now = date.today()
+    datetime=now.strftime('%b-%d-%Y-%H-%M-%S')
+    hist.write(str(datetime)+','+str(last_price)+'\n')
+    hist.flush()
+    time.sleep(15)
+except KeyboardInterrupt:
+  hist.close()
